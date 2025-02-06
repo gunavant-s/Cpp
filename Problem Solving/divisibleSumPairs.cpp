847bf8ee7ac0f291e0d5d238eb9c79a21a5b7156
Gunavant-S <139606020+gunavant-s@users.noreply.github.com>
2025-02-06 08:24:11 -0500

    https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?

--- Problem Solving/divisibleSumPairs.cpp
+++ Problem Solving/divisibleSumPairs.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -19,7 +18,7 @@ vector<string> split(const string &);
 
 int divisibleSumPairs(int n, int k, vector<int> ar) {
     int result = 0;
-    for(int i = 0;i<ar.size()-1;i++){
+    for(int i = 0;i<ar.size();i++){
         for(int j = i+1;j<ar.size();j++){
             if((ar[i]+ar[j]) % k == 0){
                 result++;
